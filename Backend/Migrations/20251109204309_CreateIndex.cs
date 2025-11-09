707d7b6d5f1438289804834636f4526acfddaa11
Gabriel Araújo Lira <gabrielaraujolira12@gmail.com>
2025-11-09 17:56:15 -0300

    Cria Migrations

---
+++ Backend/Migrations/20251109204309_CreateIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kalenner.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_CompanyId",
+                table: "Appointments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CompanyId_ProfessionalId_Start",
+                table: "Appointments",
+                columns: new[] { "CompanyId", "ProfessionalId", "Start" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Appointments_CompanyId_ProfessionalId_Start",
+                table: "Appointments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CompanyId",
+                table: "Appointments",
+                column: "CompanyId");
+        }
+    }
+}
